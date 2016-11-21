556556a24e83d56774e22f3d7e3672fe823c7cc8
Drzewiecki <bartosz.drzewiecki@atos.net>
2016-11-21 20:22:47 +0100

    add MDL package

--- src/app/Container.jsx
+++ src/app/Container.jsx
@@ -3,7 +3,15 @@ import React, {Component} from "react";
 export class App extends Component {
     render() {
         return (
-            <h1>Hello there!</h1>
+            <main>
+                <h1>Hello there!</h1>
+                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    Button
+                </button>
+                <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                    <i className="material-icons">add</i>
+                </button>
+            </main>
         );
     }
 }
