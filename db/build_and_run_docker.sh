1d01a511668b4678e328d40dcd26c14b285ea26c
klubkodowania <klubkodowania@wp.pl>
2017-06-16 20:05:04 +0200

    Added projects to the mocked db and fixed flow in the docker initialization.

--- db/build_and_run_docker.sh
+++ db/build_and_run_docker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+docker stop ccm-db
+
+docker rm ccm-db
+
 docker rmi ccm-mongo-image
 
 docker build -t ccm-mongo-image .
 
 echo "Image created."
-
-docker rm ccm-db
 
 docker run -d -p 27017:27017 --name ccm-db ccm-mongo-image
 
@@ -14,6 +16,7 @@ echo "Container started with."
 
 docker exec ccm-db mongoimport --db ccm --collection students --file /tmp/students.json --type json --jsonArray
 docker exec ccm-db mongoimport --db ccm --collection semesters --file /tmp/semesters.json --type json --jsonArray
+docker exec ccm-db mongoimport --db ccm --collection projects --file /tmp/projects.json --type json --jsonArray
 
 echo "Seed data imported to the ccm db."
 
